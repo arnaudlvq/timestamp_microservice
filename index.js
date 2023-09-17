f4930222880050234780529ab3444c5b36c6e3c9
arnaudlvq <arnaudlvq@gmail.com>
2023-09-17 22:15:22 +0200

    first commit

--- index.js
+++ index.js
@@ -19,11 +19,10 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
 });
-
 
 
 // listen for requests :)
